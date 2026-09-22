4be51c95fc96b5d683edc07dab7a74098b4127b8
budifelt <budi.indra94@gmail.com>
2026-09-23 00:29:24 +0700

    Fix subitem title trigger rendering

--- test/app-smoke.test.js
+++ test/app-smoke.test.js
@@ -32,6 +32,7 @@ assert.match(source, /completedAt/);
 assert.match(source, /openNoteDialog/);
 assert.match(source, /subitemDialog/);
 assert.match(source, /Subitem ditambahkan/);
+assert.match(source, /renderSubitemCells/);
 assert.match(source, /Note/);
 assert.match(source, /has-note/);
 assert.match(markup, /noteDialog/);
